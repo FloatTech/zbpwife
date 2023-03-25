dc5f637a4c7526e277733bda9779c288595c60bf
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-03-25 17:09:22 +0800

    合入主仓库

--- README.md
+++ README.md
@@ -1,19 +1,6 @@
 # zbpwife
 
-用于ZeroBot-Plugin的老婆库
+用于 ZeroBot-Plugin wife 插件的老婆库
 
-## 使用方法
-
-在zbp主程序中导入此包，并将wives文件夹改名为Wives放入zbp/data目录下
-
-## 自定义老婆库
-
-将以`{Waifu名字}.{后缀名}`命名的文件加入`wives`/`Wives`文件夹后，运行`python3 genjson.py`即可将生成新的老婆库！
-
-## 征集台词
-
-wife.json中预留下了角色台词的空间，欢迎pr加入更多内容！
-
-## TODO
-
-- [ ] 去Python化，改成使用Go生成YAML文件）
+## 贡献方法
+在`wives`文件夹添加图片，运行`main.go`更新`wife.json`后PR。
--- main.go
+++ main.go
@@ -1,67 +1,59 @@
-// Package wife 抽老婆
-package wife
+package main
 
 import (
 	"encoding/json"
-	"strconv"
+	"os"
+	"strings"
 
-	fcext "github.com/FloatTech/floatbox/ctxext"
-	"github.com/FloatTech/floatbox/file"
-	ctrl "github.com/FloatTech/zbpctrl"
-	"github.com/FloatTech/zbputils/control"
-	"github.com/FloatTech/zbputils/ctxext"
-	"github.com/sirupsen/logrus"
-	zero "github.com/wdvxdr1123/ZeroBot"
-	"github.com/wdvxdr1123/ZeroBot/message"
+	"github.com/fumiama/imgsz"
 )
 
-type cardInfo struct {
-	Name  string   `json:"name"`
-	URL   string   `json:"url"`
-	Lines []string `json:"lines"`
-}
-type cardSet = map[string]cardInfo
+//go:generate go run main.go
 
-var cardMap = make(cardSet, 50)
-var datapath string
+const wifvesdir = "./wives/"
+const jsonfile = "wife.json"
 
-func init() {
-	engine := control.Register("wife", &ctrl.Options[*zero.Ctx]{
-		DisableOnDefault: false,
-		Help: "抽老婆\n" +
-			"- 抽老婆",
-		PublicDataFolder: "Wives",
-	}).ApplySingle(ctxext.DefaultSingle)
-	engine.OnFullMatchGroup([]string{"抽老婆"}, fcext.DoOnceOnSuccess(
-		func(ctx *zero.Ctx) bool {
-			data, err := engine.GetLazyData("wife.json", true)
+func main() {
+	ent, err := os.ReadDir(wifvesdir)
+	if err != nil {
+		panic(err)
+	}
+	cards := make([]string, 0, len(ent))
+	for _, en := range ent {
+		if en.IsDir() {
+			continue
+		}
+		name := en.Name()
+		fn := wifvesdir + name
+		f, err := os.Open(fn)
+		if err != nil {
+			continue
+		}
+		_, format, err := imgsz.DecodeSize(f)
+		_ = f.Close()
+		if err != nil {
+			continue
+		}
+		i := strings.LastIndex(name, ".")
+		if i <= 0 {
+			continue
+		}
+		name = name[:i] + "." + format
+		nfn := wifvesdir + name
+		if fn != nfn {
+			err = os.Rename(fn, nfn)
 			if err != nil {
-				ctx.SendChain(message.Text("ERROR:", err))
-				return false
+				continue
 			}
-			err = json.Unmarshal(data, &cardMap)
-			if err != nil {
-				ctx.SendChain(message.Text("ERROR:", err))
-				return false
-			}
-			datapath = file.BOTPATH + "/" + engine.DataFolder()
-			logrus.Infof("[wife]读取%d个老婆", len(cardMap))
-			return true
-		},
-	)).SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			i := fcext.RandSenderPerDayN(ctx.Event.UserID, len(cardMap))
-			card := cardMap[(strconv.Itoa(i))]
-
-			if id := ctx.SendChain(
-				message.At(ctx.Event.UserID),
-				message.Text("今天的二次元老婆是~【", card.Name, "】哒"),
-				message.Image("file:///"+datapath+card.URL),
-				// message.Text("\n【", card.Lines[rand.Intn(len(card.Lines))], "】"),
-			); id.ID() == 0 {
-				ctx.SendChain(
-					message.At(ctx.Event.UserID),
-					message.Text("今天的二次元老婆是~【", card.Name, "】哒\n【图片发送失败，请联系维护者~】"))
-			}
-		})
+		}
+		cards = append(cards, name)
+	}
+	f, err := os.Create(jsonfile)
+	if err != nil {
+		panic(err)
+	}
+	err = json.NewEncoder(f).Encode(cards)
+	if err != nil {
+		panic(err)
+	}
 }
